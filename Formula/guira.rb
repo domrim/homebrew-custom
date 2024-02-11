a0e944747f7bb1b83105f0733184cc22284775a1
Dominik Rimpf <dev@drimpf.de>
2024-02-11 01:39:25 +0100

    fix: guira java home

--- Formula/guira.rb
+++ Formula/guira.rb
@@ -3,7 +3,7 @@ class Guira < Formula
   homepage "https://blog.pki.dfn.de/tag/guira-releases/"
   url "https://pki.pca.dfn.de/guira/guira-3.9.1.zip"
   sha256 "457cbd8aa86ff91a173ae86ea44cc8d55967ffe2528ccea81f3fcdff6f836161"
-  license ""
+  revision 1
 
   depends_on "openjdk"
 
@@ -15,6 +15,7 @@ class Guira < Formula
     inreplace bin/"guira" do |s|
       s.gsub!("./lib", libexec)
       s.sub!(/^dir=.*$/, "dir=#{opt_prefix}")
+      s.sub!(/^# JAVA_HOME=.*$/, "JAVA_HOME=#{HOMEBREW_PREFIX}/opt/openjdk")
     end
 
     inreplace libexec/"guira-env.txt" do |s|
