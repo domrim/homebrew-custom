9caf22c1f037b1ebe63659a6fc44ea5ab5fc286d
Dominik Rimpf <dev@drimpf.de>
2024-10-06 00:13:19 +0200

    add tests

--- Formula/blisp.rb
+++ Formula/blisp.rb
@@ -20,15 +20,13 @@ class Blisp < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test blisp`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system bin/"program", "do", "something"`.
-    system "false"
+    output = shell_output("#{bin}/blisp write --chip=bl60x --reset --port COMX name_of_firmware.bin 2>&1", 4)
+    assert_match "Failed to open device", output
+
+    output = shell_output("#{bin}/blisp write --chip=bl70x name_of_firmware.bin 2>&1", 3)
+    assert_match "Device not found", output
+
+    assert_match version.to_s, shell_output("#{bin}/blisp --version")
+    assert_match "Writes firmware to SPI Flash", shell_output("#{bin}/blisp write --help")
   end
 end
