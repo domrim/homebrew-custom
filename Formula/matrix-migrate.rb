94674b6aebe92eb8c1e8208143f1021cfba435b8
Dominik Rimpf <dev@drimpf.de>
2026-05-01 18:58:38 +0200

    update matrix-migrate and enable shell completion

--- Formula/matrix-migrate.rb
+++ Formula/matrix-migrate.rb
@@ -1,14 +1,17 @@
 class MatrixMigrate < Formula
   desc "CLI to migrate one matrix account to a new one"
   homepage "https://gitlab.com/domrim/matrix-migrate"
-  url "https://gitlab.com/domrim/matrix-migrate/-/archive/v0.3.0/matrix-migrate-v0.3.0.tar.gz"
-  sha256 "2a67dafe2b29de9648fc09df8240bdf2a75a56b425a087e42deb653f52e77ee4"
+  url "https://gitlab.com/domrim/matrix-migrate/-/archive/v0.4.0/matrix-migrate-v0.4.0.tar.gz"
+  sha256 "ce138356bf07818c274e23383db537f58ad4925e6ed777e62ae609de8905f43d"
   license "MIT"
+  head "https://gitlab.com/domrim/matrix-migrate.git", branch: "main"
 
   depends_on "rustup" => :build
 
   def install
     system "cargo", "install", *std_cargo_args
+
+    generate_completions_from_executable(bin/"matrix-migrate", "completions")
   end
 
   test do
