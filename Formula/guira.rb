585cf7d07ef6d65ffe67529b44ed5de96c848656
Dominik Rimpf <dev@drimpf.de>
2026-05-11 15:36:26 +0200

    add livecheck for guira

--- Formula/guira.rb
+++ Formula/guira.rb
@@ -5,6 +5,14 @@ class Guira < Formula
   url "https://pki.pca.dfn.de/guira/guira-4.2.zip"
   sha256 "577d5da44c649565290071bfe268f2e268778a2425756c3c9c97b80c059d791d"
 
+  livecheck do
+    url "https://blog.pki.dfn.de/feed/"
+    regex(/Java RA-Oberfläche Version ?(\d+(?:\.\d+)+)/i)
+    strategy :xml do |xml, regex|
+      xml.get_elements("rss//channel//item//title").map { |item| item.text[regex, 1] }
+    end
+  end
+
   depends_on "openjdk"
 
   def install
