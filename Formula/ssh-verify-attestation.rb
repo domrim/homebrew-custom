3d7455d93f9fcaae943db3006243b896cc82c944
Dominik Rimpf <dev@drimpf.de>
2026-05-19 14:24:02 +0200

    add ssh-verify-attestation

---
+++ Formula/ssh-verify-attestation.rb
@@ -0,0 +1,64 @@
+class SshVerifyAttestation < Formula
+  desc "OpenBSD freely-licensed SSH connectivity tools"
+  homepage "https://www.openssh.com/"
+  url "https://cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-10.3p1.tar.gz"
+  mirror "https://cloudflare.cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-10.3p1.tar.gz"
+  version "10.3p1"
+  sha256 "56682a36bb92dcf4b4f016fd8ec8e74059b79a8de25c15d670d731e7d18e45f4"
+  license "SSH-OpenSSH"
+  compatibility_version 1
+
+  livecheck do
+    url "https://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/"
+    regex(/href=.*?openssh[._-]v?(\d+(?:\.\d+)+(?:p\d+)?)\.t/i)
+  end
+
+  # Please don't resubmit the keychain patch option. It will never be accepted.
+  # https://archive.is/hSB6d#10%25
+
+  depends_on "pkgconf" => :build
+  depends_on "ldns"
+  depends_on "libfido2"
+  depends_on "openssl@3"
+
+  uses_from_macos "mandoc" => :build
+  uses_from_macos "lsof" => :test
+  uses_from_macos "krb5"
+  uses_from_macos "libedit"
+  uses_from_macos "libxcrypt"
+
+  on_linux do
+    depends_on "linux-pam"
+    depends_on "zlib-ng-compat"
+  end
+
+  resource "com.openssh.sshd.sb" do
+    url "https://raw.githubusercontent.com/apple-oss-distributions/OpenSSH/OpenSSH-268.100.4/com.openssh.sshd.sb"
+    sha256 "a273f86360ea5da3910cfa4c118be931d10904267605cdd4b2055ced3a829774"
+  end
+
+  def install
+    ENV.append "CPPFLAGS", "-D__APPLE_SANDBOX_NAMED_EXTERNAL__" if OS.mac?
+
+    args = %W[
+      --sysconfdir=#{etc}/ssh
+      --with-ldns
+      --with-libedit
+      --with-kerberos5
+      --with-pam
+      --with-ssl-dir=#{Formula["openssl@3"].opt_prefix}
+      --with-security-key-builtin
+    ]
+
+    args << "--with-privsep-path=#{var}/lib/sshd" if OS.linux?
+
+    system "./configure", *args, *std_configure_args
+    system "make ssh-verify-attestation"
+
+    bin.install "regress/misc/ssh-verify-attestation/ssh-verify-attestation" => "ssh-verify-attestation"
+  end
+
+  test do
+    false
+  end
+end
